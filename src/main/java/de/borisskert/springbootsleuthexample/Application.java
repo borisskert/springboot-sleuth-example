a109782bda1eca49dcd2c294a8db73cb87711ba9
Boris Skert <boris.skert@gmail.com>
2022-06-09 16:22:42 +0200

    Clean up project

--- src/main/java/de/borisskert/springbootsleuthexample/Application.java
+++ src/main/java/de/borisskert/springbootsleuthexample/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootSleuthExampleApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootSleuthExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
