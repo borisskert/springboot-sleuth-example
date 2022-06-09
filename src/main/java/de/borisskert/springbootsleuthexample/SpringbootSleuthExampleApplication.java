9bd97944c6234afc440406aed66938d13bcd8e95
Boris Skert <boris.skert@gmail.com>
2022-06-09 16:21:43 +0200

    Create project with Spring Initializr

---
+++ src/main/java/de/borisskert/springbootsleuthexample/SpringbootSleuthExampleApplication.java
@@ -0,0 +1,13 @@
+package de.borisskert.springbootsleuthexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSleuthExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSleuthExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/borisskert/springbootsleuthexample/SpringbootSleuthExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.borisskert.springbootsleuthexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSleuthExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
